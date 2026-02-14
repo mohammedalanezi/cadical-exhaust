0db8858455f137dd5dfa755b9c0260723fb079be
Mohammed Al-Anezi <228255292+mohammedalanezi@users.noreply.github.com>
2026-02-14 17:19:08 -0500

    Changed --order to --observe to better describe the argument

---
+++ exhaustive.cpp
@@ -0,0 +1,143 @@
+#include "exhaustive.hpp"
+#include <iostream>
+#include <deque>
+#include <cassert>
+
+ExhaustiveSearch::ExhaustiveSearch(CaDiCaL::Solver * s, std::vector<int> to_observe, bool only_neg, FILE * solfile) : solver(s) {
+    if (to_observe.empty())
+    {
+        for(int i=0; i < s->vars(); i++)
+            observed.push_back(i+1);
+    } // No order provided; run exhaustive search on all variables
+    else 
+        observed = to_observe;
+
+    this->only_neg = only_neg;
+    this->solfile = solfile;
+    
+    assignment.assign(s->vars(), 0);
+    assignments_by_level.push_back({});
+    
+    solver->connect_external_propagator(this);
+    
+    std::cout << "c Running exhaustive search on " << observed.size() << " variables" << std::endl;
+    
+    for (int var : observed)
+        solver->add_observed_var(var);
+}
+
+ExhaustiveSearch::~ExhaustiveSearch () {
+    if (!observed.empty()) {
+        solver->disconnect_external_propagator ();
+        std::cout << "c Number of solutions: " << sol_count << std::endl;
+    }
+}
+
+void ExhaustiveSearch::notify_assignment(const std::vector<int>& lits) {
+    for (int lit : lits) { // Track assignments of observed variables
+        int idx = abs(lit) - 1;
+        if (assignment[idx] == 0) { // Variable not yet assigned
+            assignment[idx] = lit; // Store the signed literal
+            assigned_count++; // Increment total assigned variables
+            assignments_by_level.back().push_back(lit); // Store when this literal was assigned
+        }
+    }    
+    
+    if (assigned_count == observed.size() && !partial_solution_found) { // Found all assignments and no solution found
+        partial_solution_found = true;
+        block_partial_solution();
+    }
+}
+
+void ExhaustiveSearch::notify_new_decision_level () {
+    assignments_by_level.push_back({}); // Add new vector to track history
+}
+
+void ExhaustiveSearch::notify_backtrack (size_t new_level) {
+    while (assignments_by_level.size() > new_level + 1) {
+        std::vector<int>& lits_to_undo = assignments_by_level.back(); // History to wipe
+        for (int lit : lits_to_undo) {
+            int idx = abs(lit) - 1;
+            assignment[idx] = 0; 
+            assigned_count--;
+        }
+        assignments_by_level.pop_back(); // Remove history from list
+    }
+    
+    if (assigned_count < observed.size()) // No longer have all assignments
+        partial_solution_found = false;
+}
+
+void ExhaustiveSearch::block_partial_solution() {
+    sol_count++;
+
+#ifdef VERBOSE
+    if (!solfile)
+      std::cout << "c New solution: ";
+#endif
+
+    std::vector<int> clause;
+    clause.reserve(observed.size() + 1);
+    
+    for (int j = 0; j < observed.size(); j++) {
+        int var = observed[j];
+        int lit = assignment[var - 1];
+
+        if(lit==0)
+            continue;
+        
+#ifdef VERBOSE
+        if (lit > 0) {
+            if(!solfile)
+              std::cout << var << " ";
+            else
+              fprintf(solfile, "%d ", var);
+        }
+#endif
+        if (lit > 0 || !only_neg)
+            clause.push_back(-lit);
+    }
+    
+#ifdef VERBOSE
+    if(!solfile) 
+        std::cout << "0" << std::endl;
+    else 
+        fprintf(solfile, "0\n");
+#endif
+
+    new_clauses.push_back(clause);
+    solver->add_trusted_clause(clause);
+}
+
+bool ExhaustiveSearch::cb_check_found_model (const std::vector<int> & model) {
+    (void)model;
+    return false;
+}
+
+bool ExhaustiveSearch::cb_has_external_clause (bool& is_forgettable) {
+    (void)is_forgettable;
+    return !new_clauses.empty();
+}
+
+int ExhaustiveSearch::cb_add_external_clause_lit () {
+    if (new_clauses.empty()) 
+        return 0;
+    else {
+        size_t clause_idx = new_clauses.size() - 1;
+        if (new_clauses[clause_idx].empty()) {
+            new_clauses.pop_back();
+            return 0;
+        }
+
+        int lit = new_clauses[clause_idx].back();
+        new_clauses[clause_idx].pop_back();
+        return lit;
+    }
+}
+
+int ExhaustiveSearch::cb_decide () { return 0; }
+int ExhaustiveSearch::cb_propagate () { return 0; }
+int ExhaustiveSearch::cb_add_reason_clause_lit (int plit) {
+    (void)plit;
+    return 0;
+}
---
+++ exhaustive.hpp
@@ -0,0 +1,35 @@
+#include "internal.hpp"
+#include <deque>
+#include <vector>
+
+class ExhaustiveSearch : CaDiCaL::ExternalPropagator {
+    CaDiCaL::Solver * solver;
+    std::vector<std::vector<int>> new_clauses;          // Queue for blocking clauses
+    std::vector<int> assignment;                        // Current assignment: signed literal or 0 for unassigned
+    std::deque<std::vector<int>> assignments_by_level;  // History of assignment: vector of literals assigned at each level
+
+    int assigned_count = 0;                 // Number of assigned observed variables
+    bool partial_solution_found = false;    // Flag to prevent duplicate blocking
+    
+    std::vector<int> observed;
+    bool only_neg = false;
+    long sol_count = 0;
+    
+    FILE * solfile;
+    
+public:
+    ExhaustiveSearch(CaDiCaL::Solver * s, std::vector<int> to_observe, bool only_neg, FILE * solfile);
+    ~ExhaustiveSearch ();
+    void notify_assignment(const std::vector<int>& lits);
+    void notify_new_decision_level ();
+    void notify_backtrack (size_t new_level);
+    bool cb_check_found_model (const std::vector<int> & model);
+    bool cb_has_external_clause (bool& is_forgettable);
+    int cb_add_external_clause_lit ();
+    int cb_decide ();
+    int cb_propagate ();
+    int cb_add_reason_clause_lit (int plit);
+    
+private:
+    void block_partial_solution(); // Block the current partial assignment
+};
